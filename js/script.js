a10f4f788ecb2f41302db0a63db500c856df21c5
vitya8989 <vitya898989@gmail.com>
2021-11-12 00:06:50 +0300

    main

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	} else {
+		document.querySelector('body').classList.add('no-webp');
+	}
+});;
+let burger = document.querySelector('.burger');
+let nav = document.querySelector('.nav');
+
+burger.onclick = function () {
+	burger.classList.toggle('menu-open');
+	nav.classList.toggle('menu-open');
+};
