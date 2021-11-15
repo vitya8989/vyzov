7b4e68bc8eea7aff0db4b9e39377908729f975ed
vitya8989 <vitya898989@gmail.com>
2021-11-15 13:39:48 +0300

    ksdjfhsjkdf

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-function testWebP(callback) {
-
-	var webP = new Image();
-	webP.onload = webP.onerror = function () {
-		callback(webP.height == 2);
-	};
-	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
-
-testWebP(function (support) {
-
-	if (support == true) {
-		document.querySelector('body').classList.add('webp');
-	} else {
-		document.querySelector('body').classList.add('no-webp');
-	}
-});;
 let burger = document.querySelector('.burger');
 let nav = document.querySelector('.nav');
 let navLinks = document.querySelectorAll('.nav__link');
